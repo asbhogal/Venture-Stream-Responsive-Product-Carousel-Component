94574a721f5f93b915238507618411e9396915c4
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-06-04 12:59:35 +0100

    added fonts and public sub dirs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
     extend: {},
   },
   plugins: [],
