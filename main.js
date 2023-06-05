f9f28c2413363bed8673c3fa37698baa3f2e6926
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-06-05 14:59:37 +0100

    created product-card Web Component

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 import "./style.css";
+
+const template = document.createElement("template");
+
+template.innerHTML = `<img></img>
+<h3></h3>`;
+
+class ProductCard extends HTMLElement {
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: "open", delegatesFocus: true });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadowRoot.querySelector("h3").textContent =
+      this.getAttribute("title");
+    this.shadowRoot.querySelector("img").src = this.getAttribute("src");
+  }
+}
+
+customElements.define("product-card", ProductCard);
