433f13fbb3f35697f8186101a7f83574fb2636c3
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-06-06 14:04:53 +0100

    further changes to interactivity

--- src/js/carouselSlider.js
+++ src/js/carouselSlider.js
@@ -1,6 +1,30 @@
 const carousel = document.querySelector(".carousel");
-const dragging = (e) => {
-  carousel.scrollLeft = e.pageX;
+let isDragging = false;
+let startPos = 0;
+let currentPos = 0;
+
+const dragStart = (e) => {
+  isDragging = true;
+  carousel.classList.add("dragging");
+  startPos = e.pageX - carousel.offsetLeft;
 };
 
+const dragging = (e) => {
+  if (!isDragging) return;
+  e.preventDefault(); // Prevent default text selection behavior
+  currentPos = e.pageX - carousel.offsetLeft;
+  const scrollPos = currentPos - startPos;
+  carousel.scrollLeft = carousel.scrollLeft - scrollPos;
+};
+
+carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
+
+// Additional event listeners to handle mouseup and mouseleave events
+document.addEventListener("mouseup", () => {
+  isDragging = false;
+});
+
+carousel.addEventListener("mouseleave", () => {
+  isDragging = false;
+});
